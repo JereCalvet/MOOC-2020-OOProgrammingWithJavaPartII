ff703a5428af220d4a6ede37878b188b52f3f286
Jereaaa <jere_calvet@hotmail.com>
2020-06-30 20:36:00 -0300

    Ejercicio de repaso

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -1,10 +1,17 @@
-//test
+
+import java.lang.Math;
 import java.util.Scanner;
 
 public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            System.out.println((int)Math.pow(Integer.parseInt(input), 3));
+        }
     }
 }
