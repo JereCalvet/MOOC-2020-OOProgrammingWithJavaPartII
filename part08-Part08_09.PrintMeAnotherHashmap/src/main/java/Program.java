a838488734af3ab62441112236531f4e2e0a9c7e
Jereaaa <jere_calvet@hotmail.com>
2020-07-03 20:20:42 -0300

    HashMap (practicando streams)

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -3,8 +3,14 @@ import java.util.HashMap;
 
 public class Program {
 
-    public static void main(String[] args) {
-        // Test your program here!
+    public static void printValues(HashMap<String, Book> hashmap) {
+        hashmap.values().forEach(System.out::println);
     }
 
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text) {
+        hashmap.values().stream().filter(book -> book.getName().contains(text)).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+    }
 }
