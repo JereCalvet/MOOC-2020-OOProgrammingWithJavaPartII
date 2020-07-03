54746d827057fce205c26c10f311097691eec7c4
Jereaaa <jere_calvet@hotmail.com>
2020-07-03 00:21:10 -0300

    HashMap (practicando streams)

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -1,10 +1,34 @@
 
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class Program {
 
-    public static void main(String[] args) {
-        // Test your program here!
+    public static void printKeys(HashMap<String, String> hashmap) {
+        for (String key : hashmap.keySet()) {
+            System.out.println(key);
+        }
     }
 
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(key);
+            }
+        }
+//        hashmap.keySet().stream().filter(key -> key.contains(text)).findFirst().ifPresent(System.out::println);
+
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+//        hashmap.entrySet().stream().filter(value -> value.getKey().contains(text)).findFirst().map(keySet -> keySet.getValue()).ifPresent(System.out::println);
+    }
+
+    public static void main(String[] args) {
+    }
 }
