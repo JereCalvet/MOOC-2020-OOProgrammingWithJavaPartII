e5770a7789eea0bb7e7f1ecc2a4f778c8a4f2f6c
Jereaaa <jere_calvet@hotmail.com>
2020-07-20 13:09:39 -0300

    Streams y lambdas

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,15 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Write your program here
-
+        ArrayList<String> numbers = new ArrayList<>(); 
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            numbers.add(input);
+        }
+        System.out.println("Average of the numbers: " + numbers.stream().mapToInt(inputNumAsString -> Integer.valueOf(inputNumAsString)).average().getAsDouble());
     }
 }
