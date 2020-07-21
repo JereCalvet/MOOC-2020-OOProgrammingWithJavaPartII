cad25a565702984b988a9d7c9f0802ed30ea1064
Jereaaa <jere_calvet@hotmail.com>
2020-07-21 13:24:41 -0300

    Streams: forEach

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,15 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> userInputCollection = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            userInputCollection.add(input);
+        }
+        System.out.println("");
+        userInputCollection.forEach(System.out::println);
     }
 }
