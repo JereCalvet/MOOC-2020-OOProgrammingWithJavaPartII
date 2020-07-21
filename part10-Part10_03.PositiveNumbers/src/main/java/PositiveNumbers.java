a4a82836cd2e74548bc6c49eb189a6b22a17955b
Jereaaa <jere_calvet@hotmail.com>
2020-07-21 12:00:57 -0300

    Streams: Operationes intermedias y terminales

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,10 +1,16 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class PositiveNumbers {
 
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream().filter(value -> value > 0).collect(Collectors.toList());
+    //    return numbers.stream().filter(value -> value > 0).collect(Collectors.toCollection(ArrayList::new));
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
