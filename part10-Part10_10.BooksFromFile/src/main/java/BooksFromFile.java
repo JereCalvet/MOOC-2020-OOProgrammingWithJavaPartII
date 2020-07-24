7b4a7ba9692a40de5081d041712d92eeda4b81aa
Jereaaa <jere_calvet@hotmail.com>
2020-07-24 18:14:15 -0300

    File IO usando stream

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -7,11 +8,23 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+
+    public static List<Book> readBooks(String file) {
+        List<Book> booksCollection = new ArrayList<>();
+        try {
+            booksCollection = Files.lines(Paths.get(file))
+                    .map(row -> row.split(",")) //file format: name,publishing year,page count,author
+                    .map(bookInfo -> new Book(bookInfo[0], Integer.valueOf(bookInfo[1]), Integer.valueOf(bookInfo[2]), bookInfo[3]))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return booksCollection;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
     }
-
 }
