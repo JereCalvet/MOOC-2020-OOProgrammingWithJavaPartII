86cc6b4eb3cdf68fa04386dab3961f15c5055cb3
Jereaaa <jere_calvet@hotmail.com>
2020-07-24 17:43:15 -0300

    File IO usando stream

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -8,10 +9,17 @@ import java.util.stream.Collectors;
 
 public class ReadingFilesPerLine {
 
+    public static List<String> read(String file) {
+        List<String> fileAsStream = new ArrayList<>();
+        try {
+            fileAsStream = Files.lines(Paths.get(file)).collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return fileAsStream;
+    } 
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test the method here
-
     }
-
 }
