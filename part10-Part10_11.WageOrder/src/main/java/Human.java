5b41e225852dd4c30c319e2ac965def7190f0458
Jereaaa <jere_calvet@hotmail.com>
2020-07-25 19:02:28 -0300

    Interfaz: Comparable<>

--- part10-Part10_11.WageOrder/src/main/java/Human.java
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human> {
 
     private int wage;
     private String name;
@@ -17,9 +17,13 @@ public class Human {
         return wage;
     }
 
-
     @Override
     public String toString() {
         return name + " " + wage;
+    }
+
+    @Override
+    public int compareTo(Human otherHuman) {
+        return otherHuman.getWage() - this.wage;
     }
 }
