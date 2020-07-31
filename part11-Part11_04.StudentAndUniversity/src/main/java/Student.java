4d4eead31ea1e891c9773c6ffcaeb6a8b5180866
Jereaaa <jere_calvet@hotmail.com>
2020-07-31 19:40:47 -0300

    UML: Diagrama de clases con una conexión (plural   *)

---
+++ part11-Part11_04.StudentAndUniversity/src/main/java/Student.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author Jere
+ */
+public class Student {
+    private int studentID;
+    private String name;
+    private University university;
+}
---
+++ part11-Part11_04.StudentAndUniversity/src/main/java/University.java
@@ -0,0 +1,11 @@
+
+import java.util.List;
+
+/**
+ *
+ * @author Jere
+ */
+public class University {
+    private String name;
+    private List<Student> students;
+}
