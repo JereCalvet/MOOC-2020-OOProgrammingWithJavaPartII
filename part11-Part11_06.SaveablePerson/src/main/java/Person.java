3137582217910e4ca010da361024fbd4886ead63
Jereaaa <jere_calvet@hotmail.com>
2020-08-01 19:04:38 -0300

    UML: Diagrama de clases implementando una interfaz

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Jere
+ */
+public class Person implements Saveable {
+    private String name;
+    private String address;
+
+    @Override
+    public void save() {
+    }
+
+    @Override
+    public void delete() {
+    }
+
+    @Override
+    public void load(String address) {
+    }    
+}
---
+++ part11-Part11_06.SaveablePerson/src/main/java/Saveable.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author Jere
+ */
+public interface Saveable {
+    void save();
+    void delete();
+    void load(String address);
+}
