68453a80a27107bef785b0497da193caf84faab5
Jereaaa <jere_calvet@hotmail.com>
2020-08-05 17:59:20 -0300

    Exceptions: Illegal arguments

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,7 +3,9 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
-
+        if (num < 0) {
+            throw new IllegalArgumentException("factorial argument exception");
+        }
         int answer = 1;
         for (int i = 1; i <= num; i++) {
             answer *= i;
@@ -13,7 +15,9 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
-
+        if (setSize < 0 || subsetSize < 0 || subsetSize > setSize) {
+            throw new IllegalArgumentException("binomial coeficient arguments exception.");
+        }
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
 
