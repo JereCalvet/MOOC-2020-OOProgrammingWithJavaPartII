884327d8ea85b68f4186bec6f9d102e68787dbf2
Jereaaa <jere_calvet@hotmail.com>
2020-08-10 20:08:45 -0300

    Ejercicio array

--- part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -1,12 +1,18 @@
 
-import java.util.Scanner;
-
 public class Program {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // test your method here
-
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        if (fromWhere < 0) fromWhere = 0;
+        if (toWhere > array.length) toWhere = array.length;
+        int sum = 0;
+        for (; fromWhere < toWhere; fromWhere++) {
+            if (array[fromWhere] >= smallest && array[fromWhere] <= largest) {
+                sum += array[fromWhere];
+            }
+        }
+        return sum;
     }
-
+    
+    public static void main(String[] args) {
+    }
 }
