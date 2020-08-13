20c09afba5f9e6a9777f7bff1ed3e240556aa8e1
Jereaaa <jere_calvet@hotmail.com>
2020-08-13 20:48:31 -0300

    Custom hashmap

--- part12-Part12_05.HashMap/src/main/java/Program.java
+++ part12-Part12_05.HashMap/src/main/java/Program.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // You can test the class here
-
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.add(1, "uno");
+        map.add(2, "dos");
+        map.add(3, "tres");
+        
+        System.out.println(map.get(1));
+        System.out.println(map.get(3));
+        System.out.println(map.get(2));
+        System.out.println(map.get(4));
+        map.remove(2);
+        System.out.println(map.get(2));
+        
     }
 
 }
