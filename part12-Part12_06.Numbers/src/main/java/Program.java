1389bb1763241ad5848eeb1d30c22157dae22324
Jereaaa <jere_calvet@hotmail.com>
2020-08-14 12:31:54 -0300

    Class random

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,12 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("How many random numbers should be printed? ");
+        int amount = Integer.valueOf(scanner.nextLine());
+        while (amount-- > 0) {
+            System.out.println(random.nextInt(11));
+        }
     }
 
 }
