38b053c6e5f7d3d58ffc3b1afcb8b5ff9a3807cc
Jereaaa <jere_calvet@hotmail.com>
2020-08-19 20:19:36 -0300

    GUI: JavaFX

--- part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
+++ part13-Part13_01.MyFirstApplication/src/main/java/myFirstApplication/MyFirstApplication.java
@@ -1,11 +1,17 @@
 package myFirstApplication;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class MyFirstApplication {
+public class MyFirstApplication extends Application {
     
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(MyFirstApplication.class);
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("My first application");
+        stage.show();
+    }
 }
