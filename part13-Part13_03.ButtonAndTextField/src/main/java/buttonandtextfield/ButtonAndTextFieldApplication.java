f33b0395a76cf97f887014648ab780ed864e35e3
Jereaaa <jere_calvet@hotmail.com>
2020-08-20 20:17:56 -0300

    GUI: JavaFX. Button y text field.

--- part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,32 @@
 package buttonandtextfield;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndTextFieldApplication {
+public class ButtonAndTextFieldApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class);
     }
 
+    private FlowPane createComponents() {
+        Button button = new Button("This is a button");
+        TextField text = new TextField("This is a text field");
+        FlowPane groupComponents = new FlowPane();
+        groupComponents.getChildren().add(button);
+        groupComponents.getChildren().add(text);
+        return groupComponents;
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Button and text field");
+        stage.setScene(new Scene(createComponents()));
+        stage.show();
+    }
 }
