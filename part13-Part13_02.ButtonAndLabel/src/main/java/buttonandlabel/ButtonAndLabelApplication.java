7435fba581864ef8feb13784ebfed9c28a8554a1
Jereaaa <jere_calvet@hotmail.com>
2020-08-20 20:18:08 -0300

    GUI: JavaFX. Button y label.

--- part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,31 @@
 package buttonandlabel;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 
-public class ButtonAndLabelApplication {
-
+public class ButtonAndLabelApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndLabelApplication.class);
     }
 
+    private FlowPane createComponents() {
+        Button buttonComponent = new Button("Button click");
+        Label labelComponent = new Label("Press the button");
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(buttonComponent);
+        componentGroup.getChildren().add(labelComponent);
+        return componentGroup;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Button and label app");
+        stage.setScene(new Scene(createComponents()));
+        stage.show();
+    }
 }
