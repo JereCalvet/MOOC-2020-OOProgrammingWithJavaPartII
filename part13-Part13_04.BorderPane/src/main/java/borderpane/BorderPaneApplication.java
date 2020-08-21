49fd485ee0000066fa9c3060674cdc4a08292f11
Jereaaa <jere_calvet@hotmail.com>
2020-08-21 18:56:22 -0300

    GUI FX: BorderPane Layout

--- part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,33 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneApplication {
+public class BorderPaneApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(BorderPaneApplication.class);
     }
 
+    private BorderPane createComponents() {
+        Label textNorth = new Label("NORTH"); 
+        Label textEast = new Label("EAST"); 
+        Label textSouth = new Label("SOUTH"); 
+        BorderPane layout = new BorderPane();
+        layout.setTop(textNorth);
+        layout.setRight(textEast);
+        layout.setBottom(textSouth);
+        return layout;
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Border Pane Example");
+        stage.setScene(new Scene(createComponents()));
+        stage.show();
+    }
 }
