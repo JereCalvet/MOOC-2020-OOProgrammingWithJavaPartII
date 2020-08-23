6c087c65daea7ee1d341983330fca8ce9faa506e
Jereaaa <jere_calvet@hotmail.com>
2020-08-22 21:51:13 -0300

    GUI JavaFX: Event handler

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,44 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
-
+public class NotifierApplication extends Application implements EventHandler<Event> {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
     }
 
+    private VBox createComponents() {
+        VBox layout = new VBox();
+        TextField textInputTextField = new TextField();
+        Button updateButton = new Button("Update");
+        Label textOutputLabel = new Label();
+
+        updateButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                textOutputLabel.setText(textInputTextField.getText());
+            }
+        });
+       
+        layout.getChildren().add(textInputTextField);
+        layout.getChildren().add(updateButton);
+        layout.getChildren().add(textOutputLabel);
+        return layout;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Notifier application");
+        stage.setScene(new Scene(createComponents()));
+        stage.show();
+    }
 }
