4cd108277338f6140d8cf5a85a30e8cfc4681d50
Jereaaa <jere_calvet@hotmail.com>
2020-08-24 19:48:12 -0300

    GUI JavaFX: Exercise

--- part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,61 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+public class TextStatisticsApplication extends Application {
 
+    private TextStatisticsLogic logic;
 
+    public TextStatisticsApplication() {
+        this.logic = new TextStatisticsLogic();
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+    
+    private HBox statisticsOutputLayout() {
+        HBox boxLayout = new HBox();
+        boxLayout.getChildren().add(new Label("Letters: 0"));
+        boxLayout.getChildren().add(new Label("Words: 0"));
+        boxLayout.getChildren().add(new Label("The longest word is:"));
+        boxLayout.setSpacing(10);
+        
+        return boxLayout;
+    }
+    
+    private TextArea addChangeListener(TextArea textAreaComponentToChange) {
+        textAreaComponentToChange.textProperty().addListener((change, oldValue, NewValue) -> {
+            updateGUILabels(NewValue);
+        });
+        return textAreaComponentToChange;
+    }
+    
+    private BorderPane createComponents() {
+        BorderPane layout = new BorderPane();
+        TextArea inputText = new TextArea();
+        addChangeListener(inputText);
+        layout.setCenter(inputText);
+        layout.setBottom(statisticsOutputLayout());
+        
+        return layout;
+    }
+    
+    private void updateGUILabels(String textToProcess) {
+        logic.setText(textToProcess);
+        
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Text statistics application");
+        stage.setScene(new Scene(createComponents()));
+        stage.show();
+    }
 }
---
+++ part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsLogic.java
@@ -0,0 +1,50 @@
+package textstatistics;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Jere
+ */
+public class TextStatisticsLogic {
+
+    private String text;
+
+    public TextStatisticsLogic() {
+        this.text = "";
+    }
+
+    public TextStatisticsLogic(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    private String[] splitText() {
+        return text.split(" ");
+    }
+
+    public int getAmountOfWords() {
+//        if (text.isEmpty()) {
+//            return 0;
+//        }
+        return splitText().length;
+    }
+
+    public String getLongestWord() {
+        return Arrays.stream(splitText()).sorted((w1, w2) -> w2.length() - w1.length()).findFirst().orElse("");
+    }
+
+    public int getAmountOfCharacters() {
+//        if (text.isEmpty()) {
+//            return 0;
+//        }
+        return text.length();
+    }
+}
