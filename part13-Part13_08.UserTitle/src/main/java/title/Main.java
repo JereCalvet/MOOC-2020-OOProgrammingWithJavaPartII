7dfc3f59eab293593d1b9f0507d29d39c174212c
Jereaaa <jere_calvet@hotmail.com>
2020-08-25 18:01:23 -0300

    GUI JavaFX: App launch params

--- part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,16 @@
 package title;
 
+import java.util.Scanner;
+import javafx.application.Application;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert your name:");
+        String name = sc.nextLine();
+        System.out.println("Insert a title:");
+        String title = sc.nextLine();
+        Application.launch(UserTitle.class,"--User=" + name,"--Title="+title);
     }
-
 }
--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,17 @@
 package title;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class UserTitle {
+public class UserTitle extends Application {
 
-
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parameters params = getParameters();
+        String userName = params.getNamed().get("User");
+        String usertitle = params.getNamed().get("Title");
+        
+        stage.setTitle(userName + ": " + usertitle);
+        stage.show();
+    }
 }
