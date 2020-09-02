d68af3f6fd50fe26df71632a01997a0a8ded1f98
Jereaaa <jere_calvet@hotmail.com>
2020-09-02 18:05:09 -0300

    GUI JavaFX: Line Chart

--- part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
+++ part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
@@ -1,9 +1,59 @@
 package application;
 
-public class ShanghaiApplication {
+import java.util.HashMap;
+import java.util.Map;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class ShanghaiApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ShanghaiApplication.class);
     }
 
+    private Map<Integer, Integer> loadData() {
+        Map<Integer, Integer> data = new HashMap<>();
+        data.put(2007, 73);
+        data.put(2008, 68);
+        data.put(2009, 72);
+        data.put(2010, 72);
+        data.put(2011, 74);
+        data.put(2012, 73);
+        data.put(2013, 76);
+        data.put(2014, 73);
+        data.put(2015, 67);
+        data.put(2016, 56);
+        data.put(2017, 56);
+        
+        return data;
+    }
+    
+    @Override
+    public void start(Stage window) throws Exception {
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        
+        xAxis.setLabel("Year");
+        yAxis.setLabel("Ranking");
+
+        LineChart<Number, Number> graphic = new LineChart<>(xAxis, yAxis);
+        graphic.setTitle("University of Helsinki, Shangai ranking");
+
+        XYChart.Series rankingData = new XYChart.Series();
+        //rankingData.getData().add(new XYChart.Data<>(3,4));
+        rankingData.setName("Shangai ranking");
+        
+         values = rankingData;
+        
+        graphic.getData().add(rankingData);
+        
+        
+        window.setScene(new Scene(graphic, 800, 600));
+        window.setTitle("International universities ranking");
+        window.show();
+    }
 }
