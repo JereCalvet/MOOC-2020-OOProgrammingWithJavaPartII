adceefecc954e983f40979b5db0323cea1fba729
Jereaaa <jere_calvet@hotmail.com>
2020-09-04 18:36:19 -0300

    GUI JavaFX: Line Chart

--- part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -30,41 +30,67 @@ public class PartiesApplication extends Application {
         }
         return file;
     }
- /*
-Party	1968	1972	1976	1980	1984	1988	1992	1996	2000	2004	2008
-KOK	16.1	18.1	20.9	22.9	23.0	22.9	19.1	21.6	20.8	21.8	23.4
-SDP	23.9	27.1	24.8	25.5	24.7	25.2	27.1	24.5	23.0	24.1	21.2
-KESK	18.9	18.0	18.4	18.7	20.2	21.1	19.2	21.8	23.8	22.8	20.1
-VIHR	-	-	-	-	2.8	2.3	6.9	6.3	7.7	7.4	8.9
-VAS	16.9	17.5	18.5	16.6	13.1	12.6	11.7	10.4	9.9	9.6	8.8
-PS	7.3	5.0	2.1	3.0	5.3	3.6	2.4	0.9	0.7	0.9	5.4
-RKP	5.6	5.2	4.7	4.7	5.1	5.3	5.0	5.4	5.1	5.2	4.7
-*/
-    private void MapInfo() {
-        List<String> file = readFile();
+
+    private int[] getYears() {
+        String yearsAsString[] = readFile().get(0).split("\t");
+        int yearsWithOutPartyText = yearsAsString.length - 1;
+        int[] years = new int[yearsWithOutPartyText];
+
+        for (int i = 0; i < years.length; i++) {
+            years[i] = Integer.parseInt(yearsAsString[i + 1]);
+        }
         
-        Map<String, Map<Integer, Double>> infoMapped = new HashMap<>();
-        //load map with party names <party name, map<año, valor>>
-        file.stream().map(row -> row.split("\t")).forEach(row -> infoMapped.putIfAbsent(row[0], new HashMap<>()));
-        infoMapped.remove("Party");
-        String years = file.get(0);
-        years.split("\t")
-        infoMapped.get(0)
+        return years;
     }
-    
-    private List<XYChart.Series> handleFile() {
-        List<XYChart.Series> result = new ArrayList<>();
-        
+
+    private Double manipulateScore(String scoreAsString) {
+        try {
+            return Double.valueOf(scoreAsString);
+        } catch (NumberFormatException e) {
+            return 0.0;
+        }
+    }
+
+    private Map<Integer, Double> getMapYearScore(String partyName) {
+        Map<Integer, Double> yearScore = new HashMap<>();
+        int[] years = getYears();
+
+        readFile().stream()
+                .map(row -> row.split("\t"))
+                .forEach(row -> {
+                    if (row[0].equals(partyName)) {
+                        for (int i = 1; i < row.length; i++) {
+                            yearScore.put(years[i - 1], manipulateScore(row[i]));
+                        }
+                    }
+                });
+        return yearScore;
+    }
+
+    private Map<String, Map<Integer, Double>> mapInfo() {
         List<String> file = readFile();
+        Map<String, Map<Integer, Double>> infoMapped = new HashMap<>();
         
-        
-        file.stream().map(row -> row.split("\t")).forEach(line -> {
-            XYChart.Series kokData = new XYChart.Series();
-            kokData.setName(line[0]);
-            kokData.getData().add(new XYChart.Data<>(line, line));
-            result.add(kokData);
+        file.stream().map(row -> row.split("\t"))
+                .forEach(row -> infoMapped.putIfAbsent(row[0], getMapYearScore(row[0])));
+        infoMapped.remove("Party");
+
+        return infoMapped;
+    }
+
+    private void addInfoToGraphic(LineChart<Number, Number> graphic) {
+        Map<String, Map<Integer, Double>> values = mapInfo();
+
+        values.keySet().stream().forEach(party -> {
+            XYChart.Series data = new XYChart.Series();
+            data.setName(party);
+
+            values.get(party).entrySet().stream().forEach(pair -> {
+                data.getData().add(new XYChart.Data(pair.getKey(), pair.getValue()));
+            });
+
+            graphic.getData().add(data);
         });
-        return result;
     }
 
     @Override
@@ -74,8 +100,9 @@ RKP	5.6	5.2	4.7	4.7	5.1	5.3	5.0	5.4	5.1	5.2	4.7
         xAxies.setLabel("Year");
         yAxies.setLabel("Support");
 
-        LineChart graphic = new LineChart(xAxies, yAxies);
+        LineChart<Number, Number> graphic = new LineChart<>(xAxies, yAxies);
         graphic.setTitle("Relative support of the parties");
+        addInfoToGraphic(graphic);
 
         window.setScene(new Scene(graphic, 800, 600));
         window.setTitle("Finnish parties application");
