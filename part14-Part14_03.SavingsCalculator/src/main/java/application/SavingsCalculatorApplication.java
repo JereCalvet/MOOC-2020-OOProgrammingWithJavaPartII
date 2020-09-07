23d235aab8524bd990a4223deedf584027a7f01b
Jereaaa <jere_calvet@hotmail.com>
2020-09-07 19:48:49 -0300

    GUI JavaFX: Line Chart and Sliders

--- part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
+++ part14-Part14_03.SavingsCalculator/src/main/java/application/SavingsCalculatorApplication.java
@@ -1,5 +1,8 @@
 package application;
 
+import application.logic.SavingsCalculator;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -12,7 +15,18 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SavingsCalculatorApplication extends Application {
+    
+    private SavingsCalculator calculatorLogic;
 
+    public SavingsCalculatorApplication() {
+        this.calculatorLogic = new SavingsCalculator();
+    }
+    
+    private String roundDouble(Double valueToRound) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        return df.format(valueToRound.doubleValue());
+    }
     public static void main(String[] args) {
         launch(SavingsCalculatorApplication.class);
     }
@@ -20,31 +34,41 @@ public class SavingsCalculatorApplication extends Application {
     @Override
     public void start(Stage window) throws Exception {
         NumberAxis xAxis = new NumberAxis(0, 30, 1);
-        NumberAxis yAxis = new NumberAxis();//calculate params (min, max, jump)
+        NumberAxis yAxis = new NumberAxis();//calculate params (min, max, jump
+        xAxis.setLabel("Years");
+        yAxis.setLabel("Savings");
         LineChart<Number, Number> graphic = new LineChart<>(xAxis, yAxis);
-
+        graphic.setTitle("Savings counter");
+        
+        
+        
         VBox dragSliders = new VBox();
         BorderPane monthlySavingsLayout = new BorderPane();
         monthlySavingsLayout.setPadding(new Insets(20, 20, 20, 20));
+        Label monthlySavingAmountDisplayLabel = new Label("25.0");
         Slider monthlySavingSlider = new Slider(25, 250, 25);
         monthlySavingSlider.setShowTickLabels(true);
         monthlySavingSlider.setShowTickMarks(true);
         monthlySavingsLayout.setLeft(new Label("Monthly savings"));
         monthlySavingsLayout.setCenter(monthlySavingSlider);
-        monthlySavingsLayout.setRight(new Label(String.valueOf(monthlySavingSlider.getValue())));
+        monthlySavingsLayout.setRight(monthlySavingAmountDisplayLabel);
+        monthlySavingSlider.valueProperty().addListener((changed, oldValue, newValue) -> monthlySavingAmountDisplayLabel.setText(roundDouble(newValue.doubleValue()))); 
+        monthlySavingSlider.setOnMouseReleased(event -> System.out.println("test"));
         
         BorderPane anualTaxRateLayout = new BorderPane();
         anualTaxRateLayout.setPadding(new Insets(20, 20, 20, 20));
+        Label anualTaxRateAmountDisplayLabel = new Label("0.0");
         Slider anualTaxRateSlider = new Slider(0, 10, 0);
         anualTaxRateSlider.setShowTickMarks(true);
         anualTaxRateSlider.setShowTickLabels(true);
         anualTaxRateLayout.setLeft(new Label("Yearly interest rate"));
         anualTaxRateLayout.setCenter(anualTaxRateSlider);
-        anualTaxRateLayout.setRight(new Label(String.valueOf(anualTaxRateSlider.getValue())));
-        
+        anualTaxRateLayout.setRight(anualTaxRateAmountDisplayLabel);
+        anualTaxRateSlider.valueProperty().addListener((changed, oldValue, newValue) -> anualTaxRateAmountDisplayLabel.setText(roundDouble(newValue.doubleValue()))); 
+      
         dragSliders.getChildren().add(monthlySavingsLayout);
         dragSliders.getChildren().add(anualTaxRateLayout);
-        
+
         BorderPane mainLayout = new BorderPane();
         mainLayout.setCenter(graphic);
         mainLayout.setTop(dragSliders);
---
+++ part14-Part14_03.SavingsCalculator/src/main/java/application/logic/SavingsCalculator.java
@@ -0,0 +1,67 @@
+package application.logic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Jere
+ */
+public class SavingsCalculator {
+
+    private Double monthlySavings;
+    private Double yearlyInterestRate;
+    Map<Integer, Double> nextYearsAmountNoRates;
+    Map<Integer, Double> nextYearsAmountWithRates;
+
+    public SavingsCalculator() {
+        monthlySavings = 0.0;
+        yearlyInterestRate = 0.0;
+        nextYearsAmountNoRates = new HashMap<>();
+        nextYearsAmountWithRates = new HashMap<>();
+    }
+
+    public void setMonthlySavings(Double monthlySavings) {
+        this.monthlySavings = monthlySavings;
+    }
+
+    public void setYearlyInterestRate(Double yearlyInterestRate) {
+        this.yearlyInterestRate = yearlyInterestRate / 100;
+    }
+
+    public Double getMonthlySavings() {
+        return monthlySavings;
+    }
+
+    public Double getYearlyInterestRate() {
+        return yearlyInterestRate;
+    }
+
+    public Map<Integer, Double> getNextYearsAmountNoRates() {
+        return nextYearsAmountNoRates;
+    }
+
+    public Map<Integer, Double> getNextYearsAmountWithRates() {
+        return nextYearsAmountWithRates;
+    }
+
+    public void calculateYearlyAmountNoRates() {
+        for (int years = 1; years <= 30; years++) {
+            nextYearsAmountNoRates.put(years, monthlySavings * years * 12);
+        }
+    }
+
+    public void calculateYearlyAmountWithRates() {
+        Double savedInAYear = monthlySavings * 12;
+        Double currentAmount = 0.0;       
+                 
+        for (int years = 1; years <= 30; years++) {
+            currentAmount = (savedInAYear + currentAmount) * (yearlyInterestRate + 1);
+            nextYearsAmountWithRates.put(years, currentAmount);
+        }
+    }
+
+    public void test() {
+        nextYearsAmountWithRates.entrySet().stream().forEach(entry -> System.out.println("Year: " + entry.getKey() + " Amount: " + entry.getValue()));
+    }
+}
