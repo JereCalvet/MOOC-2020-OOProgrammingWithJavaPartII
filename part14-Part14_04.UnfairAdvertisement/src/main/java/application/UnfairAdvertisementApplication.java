30e803a569bacaabe6cde70e2d1cb25544fd3735
Jereaaa <jere_calvet@hotmail.com>
2020-09-10 17:26:18 -0300

    GUI JavaFX: Bar Chart

--- part14-Part14_04.UnfairAdvertisement/src/main/java/application/UnfairAdvertisementApplication.java
+++ part14-Part14_04.UnfairAdvertisement/src/main/java/application/UnfairAdvertisementApplication.java
@@ -13,8 +13,8 @@ public class UnfairAdvertisementApplication extends Application {
     @Override
     public void start(Stage window) {
         CategoryAxis xAxis = new CategoryAxis();
-        NumberAxis yAxis = new NumberAxis(77.0, 77.5, 0.1);
-        yAxis.setTickLabelsVisible(false);
+        NumberAxis yAxis = new NumberAxis(70.0, 80.0, 0.5);
+        yAxis.setTickLabelsVisible(true);
         yAxis.setLabel("Faster and Better!");
 
         BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
@@ -23,10 +23,11 @@ public class UnfairAdvertisementApplication extends Application {
         barChart.setLegendVisible(false);
 
         XYChart.Series speeds = new XYChart.Series();
-        speeds.getData().add(new XYChart.Data("NDA", 77.4));
         speeds.getData().add(new XYChart.Data("Fastie", 77.2));
-        speeds.getData().add(new XYChart.Data("SuperNet", 77.1));
         speeds.getData().add(new XYChart.Data("Meganet", 77.1));
+        speeds.getData().add(new XYChart.Data("NDA", 77.4));
+        speeds.getData().add(new XYChart.Data("SuperNet", 77.1));
+        
 
         barChart.getData().add(speeds);
         Scene view = new Scene(barChart, 400, 300);
