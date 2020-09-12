84ffd96b3685324cc71b8a9a7d2334aa413ba5c2
Jereaaa <jere_calvet@hotmail.com>
2020-09-12 18:20:49 -0300

    JavaFX: Image / Image View

--- part14-Part14_07.Collage/src/main/java/collage/CollageApplication.java
+++ part14-Part14_07.Collage/src/main/java/collage/CollageApplication.java
@@ -14,6 +14,34 @@ import javafx.stage.Stage;
 
 public class CollageApplication extends Application {
 
+    private ImageView createSmallerImage() {
+        Image sourceImage = new Image("file:monalisa.png");
+
+        PixelReader imageReader = sourceImage.getPixelReader();
+
+        int width = (int) (sourceImage.getWidth() / 2);
+        int height = (int) (sourceImage.getHeight() / 2);
+
+        WritableImage targetImage = new WritableImage(width, height);
+        PixelWriter imageWriter = targetImage.getPixelWriter();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = imageReader.getColor(x*2, y*2);
+                double red = color.getRed();
+                double green = color.getGreen();
+                double blue = color.getBlue();
+                double opacity = color.getOpacity();
+
+                Color newColor = new Color(red, green, blue, opacity);
+
+                imageWriter.setColor(x, y, newColor);
+            }
+        }
+        
+        return new ImageView(targetImage);
+    }
+
     @Override
     public void start(Stage stage) {
 
@@ -54,7 +82,8 @@ public class CollageApplication extends Application {
 
         Pane pane = new Pane();
         pane.getChildren().add(image);
-
+        pane.getChildren().add(createSmallerImage());
+        
         stage.setScene(new Scene(pane));
         stage.show();
     }
