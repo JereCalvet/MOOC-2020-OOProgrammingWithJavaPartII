619b06c5d4729890ff577abbe8d2677795a2d675
Jereaaa <jere_calvet@hotmail.com>
2020-09-17 17:41:43 -0300

    Database: H2 in-memory DB and Dao Pattern eg.

--- part14-Part14_10.Database/src/main/java/application/UserInterface.java
+++ part14-Part14_10.Database/src/main/java/application/UserInterface.java
@@ -13,6 +13,51 @@ public class UserInterface {
         this.database = database;
     }
 
+    private void list() {
+        System.out.println("Listing the database contents \n");
+        try {
+            database.list().stream().forEach(System.out::println);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void add() {
+        System.out.println("Adding a new todo \n");
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter description: ");
+        String description = scanner.nextLine();
+
+        try {
+            database.add(new Todo(name, description, false));
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void markAsDone() {
+        System.out.println("Which todo should be marked as done (give the id)?");
+        int id = Integer.valueOf(scanner.nextLine());
+
+        try {
+            database.markAsDone(id);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void remove() {
+        System.out.println("Which todo should be removed (give the id)?");
+        int id = Integer.valueOf(scanner.nextLine());
+
+        try {
+            database.remove(id);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
     public void start() throws SQLException {
         while (true) {
             System.out.println("");
@@ -25,14 +70,28 @@ public class UserInterface {
 
             System.out.print("> ");
             String command = this.scanner.nextLine();
+            System.out.println("");
             if (command.equals("x")) {
                 break;
             }
 
-            // implement the functionality here
+            switch (command) {
+                case "1":
+                    list();
+                    break;
+                case "2":
+                    add();
+                    break;
+                case "3":
+                    markAsDone();
+                    break;
+                case "4":
+                    remove();
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
         }
-
         System.out.println("Thank you!");
     }
-
 }
